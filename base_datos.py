4bf71a89828d21ff6ce70827f61bf7959bf244d5
Adriel Padilla <164977609+AdrielApX@users.noreply.github.com>
2026-09-25 01:04:38 -0400

    Refactor database connection and user management
    Elimine la conexion a la Base de Datos

--- base_datos.py
+++ base_datos.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 # Tu conexión real al servidor de Neon.tech
-DB_URL = "postgresql://neondb_owner:npg_4owufargSh5c@ep-sparkling-cloud-ate9kxrl.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require"
+DB_URL = ""
 
 def obtener_conexion():
     return psycopg2.connect(DB_URL)
